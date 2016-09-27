eded83eb5a99b4e9f0c3f829b055f2873e0679d9
Bryan Jones <daedeross@gmail.com>
2016-09-27 14:55:56 -0500

    belt-elevator

--- src/scripts/events/building.lua
+++ src/scripts/events/building.lua
@@ -19,14 +19,25 @@ end
 
 function handle_sub_placement(event)
     local ent = event.created_entity
-    if not string.find(ent.name, "subterra_") then
+    if not string.find(ent.name, "subterra_u-") then
         local prod = ent.prototype.mineable_properties.products[1].name
         p.insert{name = prod, count = 1}
         ent.destroy()
     else
-        if ent.name == "subterra_belt-telepad" then
+        if ent.name == "subterra_belt-up" then
+            -- check above-ground spot
+            local newEnt = {
+                name = "subterra_belt-down",
+                position = ent.position,
+                direction = ent.direction,
+                force = ent.force,
+            }
+            if game.surfaces["nauvis"].can_place_entity{
+                } then
+                game.surfaces
+            end
             local proxy = {
-                entity = ent,
+                input = ent,
                 bbox = ent.bounding_box,
             }
             global.underground.belt_telepads.add_proxy(proxy)
--- src/scripts/events/tick.lua
+++ src/scripts/events/tick.lua
@@ -2,7 +2,7 @@
 
 function OnTick(event)
     local tick = event.tick
-
+    CheckBelts()
     if tick % 30 == 0 then
         CheckPads()
     end    
@@ -11,14 +11,21 @@ end
 function CheckPads()
     for _, p in pairs(global.player_proxies) do
         if p.surface.name == "underground-1" then
-            if global.underground.belt_telepads.check_proxy_collision(p.bounding_box) then
+            if global.underground.player_telepads.check_proxy_collision(p.bounding_box) then
                 p.teleport("nauvis", p.position)
             end
         elseif p.surface.name == "nauvis" then
-            if global.nauvis.belt_telepads.check_proxy_collision(p.bounding_box) then
+            if global.nauvis.player_telepads.check_proxy_collision(p.bounding_box) then
                 p.teleport("nauvis", p.position)
             end
         end
     end
 end
 
+function CheckBelts()
+    for _,b in pairs(global.belt_telepads) do
+        local line1 = b.entity.get_transport_line(1)
+        local line2 = b.entity.get_transport_line(2)
+        
+    end 
+end
